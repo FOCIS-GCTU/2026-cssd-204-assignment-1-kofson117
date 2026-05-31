9c6b073f2d96504e3547e7336f7c7b7011bb4b91
kofson117 <2425404288@live.gctu.edu.gh>
2026-05-31 10:23:05 +0100

    Add function to print formatted initials 'CEN'
    Implemented a function to print initials 'CEN' with specified formatting.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints my initials (CEN).
+# Assignment Number: Assignment Number: 1
+#
+# Name: Collins Etornam Kofi Nuworsu
+# STUDENT ID:  2425404288
+# Email: 2425404288@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Collins Etornam Kofi Nuworsu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print the initials with the exact required dimensions, formatting, and spacing.
+    print()
+    print("...CEN")
+    print()
+    print("......CCCCCC...........EEEEEEEEEEEE........NN........NN.....")
+    print("....CCCCCCCCC..........EEEEEEEEEEEE........NNN.......NN.....")
+    print("...CC......CCC.........EE..................NNNN......NN.....")
+    print("...CC.......CC.........EE..................NN.NN.....NN.....")
+    print("...CC..................EEEEEEEE............NN..NN....NN.....")
+    print("...CC..................EEEEEEEE............NN...NN...NN.....")
+    print("...CC.......CC.........EE..................NN....NN..NN.....")
+    print("...CC......CCC.........EE..................NN.....NN.NN.....")
+    print("....CCCCCCCCC.....**...EEEEEEEEEEEE...**...NN......NNNN...**")
+    print("......CCCCCC......**...EEEEEEEEEEEE...**...NN.......NNN...**")
+    print()
 
 
 main()
